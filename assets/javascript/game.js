55d1c8e9ca6ae0ec3e24839a269804b5663b8dd5
Phil S <philipstubbs13@gmail.com>
2017-12-10 16:08:11 -0600

    finally fixed issues with reset functionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -21,7 +21,12 @@ var losses = 0;
 //Create a variable to determine if user won or lost.
 var userWon;
 
-var detachButtons;
+//Create variables for crystal buttons
+var blueCrystalBtn = "<img id='blue-crystal' width= '110px' height='100px' src='../week-4-game/assets/images/blue-crystal.png'/>";
+var orangeCrystalBtn = "<img id='orange-crystal' width= '110px' height='100px' src='../week-4-game/assets/images/orange-crystal.png'/>";
+var pinkCrystalBtn = "<img id='pink-crystal' width= '110px' height='100px' src='../week-4-game/assets/images/pink-crystal.png'/>";
+var purpleCrystalBtn = "<img id='purple-crystal' width= '110px' height='100px' src='../week-4-game/assets/images/purple-crystal.png'/>";
+
 
 //Functions
 
@@ -30,6 +35,7 @@ function start () {
 	//Randomly generate number between 19 and 120.
 	targetNumber = Math.floor(Math.random() * ((120-19) + 1) + 19);
 	console.log(targetNumber);
+
 	//Display random number in HTML when game is started.
 	$("#target-number-heading").html("<h2>" + "Target number" + "</h2>").addClass("bg-primary");
 	$("#target-number").html("<h4>" + targetNumber + "</h4>");
@@ -38,148 +44,114 @@ function start () {
 	userNumber = 0;
 	$("#user-number").html("<h4>" + userNumber + "</h4>");
 
-	//Generate random number from 1 to 12 for each crystal.
-	 	blueCrystalNumber = Math.floor(Math.random() * ((12-1) + 1) + 1);
-	 	console.log(blueCrystalNumber);
- 
-	 	orangeCrystalNumber = Math.floor(Math.random() * ((12-1) + 1) + 1);
-	 	console.log(orangeCrystalNumber);
- 
-	 	pinkCrystalNumber = Math.floor(Math.random() * ((12-1) + 1) + 1);
-	 	console.log(pinkCrystalNumber);
+	//Add crystal buttons at start/reset of game.
+	$("#select-crystal-text").html("<h2 id='select-crystal-text'>" + "Select a crystal" + "</h2>");
 
-	 	purpleCrystalNumber = Math.floor(Math.random() * ((12-1) + 1) + 1);
-	 	console.log(purpleCrystalNumber);
+	//Generate random number from 1 to 12 for each crystal.
+ 	blueCrystalNumber = Math.floor(Math.random() * ((12-1) + 1) + 1);
+ 	console.log(blueCrystalNumber);
+
+ 	orangeCrystalNumber = Math.floor(Math.random() * ((12-1) + 1) + 1);
+ 	console.log(orangeCrystalNumber);
+
+ 	pinkCrystalNumber = Math.floor(Math.random() * ((12-1) + 1) + 1);
+ 	console.log(pinkCrystalNumber);
+
+ 	purpleCrystalNumber = Math.floor(Math.random() * ((12-1) + 1) + 1);
+ 	console.log(purpleCrystalNumber);
 }
 
 //Function to add random crystal numbers to user's total
 function addNumbers() {
 
-		//When blue crystal is clicked, add blueCrystalNumber to userNumber.
-		$("#blue-crystal").on("click", function() {
+	//When blue crystal is clicked, add blueCrystalNumber to userNumber.
+	$("#blue-crystal").on("click", function() {
+		if (userNumber < targetNumber) {
 			userNumber += blueCrystalNumber;
 			console.log(userNumber);
 			$("#user-number").html("<h4>" + userNumber + "</h4>");
+			checkScore();
+		}
+	});
 
-			//if userNumber equals targetNumber, user wins and game is over.
-			if (userNumber == targetNumber) {
-				userWon = true;
-				gameOver();
-			}
-
-			//If userNumber is greater than targetNumber, user loses and game is over.
-			else if (userNumber > targetNumber) {
-				userWon = false;
-				gameOver();
-			}
-		});
-
-		//When orange crystal is clicked, add orangeCrystalNumber to userNumber.
-		$("#orange-crystal").on("click", function() {
+	//When orange crystal is clicked, add orangeCrystalNumber to userNumber.
+	$("#orange-crystal").on("click", function() {
+		if (userNumber < targetNumber) {
 			userNumber += orangeCrystalNumber;
 			console.log(userNumber);
 			$("#user-number").html("<h4>" + userNumber + "</h4>");
+			checkScore();
+		}
+	});
 
-			//if userNumber equals targetNumber, user wins and game is over.
-			if (userNumber == targetNumber) {
-				userWon = true;
-				gameOver();
-			}
-
-			//If userNumber is greate than targetNumber, user loses and game is over.
-			else if (userNumber > targetNumber) {
-				userWon = false;
-				gameOver();
-			}
-		});
-
-		//When pink crystal is clicked, add pinkCrystalNumber to userNumber.
-		$("#pink-crystal").on("click", function() {
+	//When pink crystal is clicked, add pinkCrystalNumber to userNumber.
+	$("#pink-crystal").on("click", function() {
+		if (userNumber < targetNumber) {
 			userNumber += pinkCrystalNumber;
 			console.log(userNumber);
 			$("#user-number").html("<h4>" + userNumber + "</h4>");
+			checkScore();
+		}
+	});
 
-			//if userNumber equals targetNumber, user wins and game is over.
-			if (userNumber == targetNumber) {
-				userWon = true;
-				gameOver();
-			}
-
-			//If userNumber is greater than targetNumber, user loses and game is over.
-			else if (userNumber > targetNumber) {
-				userWon = false;
-				gameOver();
-			}
-		});
-
-		//When purple crystal is clicked, add purpleCrystalNumber to userNumber.
-		$("#purple-crystal").on("click", function() {
+	//When purple crystal is clicked, add purpleCrystalNumber to userNumber.
+	$("#purple-crystal").on("click", function() {
+		if (userNumber < targetNumber) {
 			userNumber += purpleCrystalNumber;
 			console.log(userNumber);
 			$("#user-number").html("<h4>" + userNumber + "</h4>");
-			
-			//if userNumber equals targetNumber, user wins and game is over.
-			if (userNumber == targetNumber) {
-				userWon = true;
-				gameOver();
-			}
+			checkScore();
+		}	
+	});	
+}
 
-			//If userNumber is greater than targetNumber, user loses and game is over.
-			else if (userNumber > targetNumber) {
-				userWon = false;
-				gameOver();
-			}
-		});	
-
-//Game over
-function gameOver() {
-	if (userWon) {
-		//When user wins, remove div that contains crystal buttons so that user cannot keep clicking them.
-		$("#select-crystal").remove();
-		//Add "You win" text.
-		$("#game-over").html("<h1>" + "You win!!" + "</h2>").addClass("jumbotron bg-primary text-white");
-		//Add win image.
-		$("#game-over").append("<img id='win-image' src='../week-4-game/assets/images/win-image.jpg'/>");
-		//Update image css properties.
-		$('#win-image').width(300).height(225).addClass("rounded-circle");
+//Compare user score to target number
+function checkScore(){
+	//if userNumber equals targetNumber, user wins and game is over.
+	if (userNumber == targetNumber) {
+		userWon = true;
 		//Add 1 to wins counter.
 		wins++;
 		$(".win-counter").html(wins);
-		//Append Play again? button
-		$("#game-over").append("<br>" + "<br>" + "<button id='reset-button'>" + "Click to play again" + "</button>");
-		$("reset-button").addClass("rounded btn btn-secondary");
-		resetGame();
+		gameOver();
 	}
 
-	else {
-		//When user loses, remove div that contains crystal buttons so that user cannot keep clicking them.
-		$("#select-crystal").detach();
-		//Add "You lose" text.
-		$("#game-over").html("<h1>" + "You went over the target number. Better luck next time." + "</h2>").addClass("jumbotron bg-primary text-white");
-		//Append lose image
-		$("#game-over").append("<img id='lose-image' src='../week-4-game/assets/images/lose-image.jpg'/>");
-		//Update image css properties.
-		$('#lose-image').width(300).height(250).addClass("rounded-circle");
-		//Add 1 to losses counter.
-		losses++;
-		$(".loss-counter").html(losses);
-		//Append Play again? button
-		$("#game-over").append("<br>" + "<br>" + "<button id='reset-button'>" + "Click to play again" + "</button>");
-		$("reset-button").addClass("rounded btn btn-secondary");
-		resetGame();
+	//If userNumber is greater than targetNumber, user loses and game is over.
+	else if (userNumber > targetNumber) {
+		userWon = false;
+ 		//Add 1 to losses counter.
+ 		losses++;
+ 		$(".loss-counter").html(losses);
+ 		gameOver();
 	}
+}
+
+//Game over
+function gameOver() {
+ 	if (userWon) {
+ 		//Append Play again? button
+ 		$("#reset-button-div").append("<button id='reset-button'>" + "Click to play again" + "</button>").append("<h2 id='win-loss-text'>" + "You win!!" + "</h2>");
+ 		$("#reset-button").addClass("round btn btn-secondary btn-block");
+ 		$("#reset-button").on("click", function() {
+ 			resetGame();
+ 		});
+ 	}
+
+ 	else {
+ 		//Append Play again? button
+ 		$("#reset-button-div").append("<button id='reset-button'>" + "Click to play again" + "</button>").append("<h2 id='win-loss-text'>" + "You went over the target number. Better luck next time." + "</h2>");
+ 		$("#reset-button").addClass("round btn btn-secondary btn-block");
+ 		$("#reset-button").on("click", function() {
+ 			resetGame();
+ 		});
+ 	}
 }
 
 //Reset game
 function resetGame () {
-	$("#reset-button").on("click", function() {
+		$("#reset-button").detach();
+		$("#win-loss-text").detach();
 		start();
-		$("#game-over").detach();
-		$('#game-over').replaceWith('#select-crystal');
-		// $('#select-crystal').append('#attachButtons');
-		//$("#select-crystal-gameover").append("<div id='select-crystal'>");
-	});
-}
 }
 
 //Execute code
